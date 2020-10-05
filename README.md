a0a809d8cff52f0f1d9492e325f5d686ed7c9e78
E-phraim <ephraimetuk85@gmail.com>
2020-10-05 09:28:26 +0100

    Presentation Phase

--- README.md
+++
@@ -1,2 +0,0 @@
-# i-Switch
-i-Switch's official website
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,45 @@
 $(document).ready(function () {
-   $(window).scroll(function () {
-       var sc = $(window).scrollTop();
-       if (sc > 100){
-           $(".navbar").addClass("sticky");
-       }
-       else{
-           $(".navbar").removeClass("sticky");
-       }
-   })
-});
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    autoplay: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
+    $(window).scroll(function () {
+        var sc = $(window).scrollTop();
+        if (sc > 100) {
+            $(".navbar").addClass("sticky");
         }
+        else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    if($(".swiper-container").hasClass("team-member-slider")){
+        var swiper = new Swiper('.swiper-container',{
+            slidesPerView:3,
+            allowTouchMove:true,
+            loop:true,
+            centeredSlides:true,
+            slideToclickedslide:true,
+            effect: "coverflow",
+            grabcursor:true,
+            autoplay:false,
+            navigation:{
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            coverflow:{
+                rotate:0,
+                stretch:100,
+                depth:200,
+                modifier:1,
+                slideShadows:false
+            },
+            breakpoints:{
+                767:{
+                    slidesPerView:1,
+                    centeredSlides:false,
+                    effect:"slide",
+                }
+            }
+        });
     }
-})
+});
 
 window.onscroll = function() {
     scrollFunction()
@@ -43,4 +56,21 @@ function scrollFunction(){
 function upFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function ISWITCH (){
+    const name = document.forms["ConForm"]["Name"]
+    const text = document.forms["ConForm"]["Text"]
+
+    if (name.value == ""){
+        window.alert('Please enter your name.');
+        name.focus();
+        return false;
+    }
+    if(text.value == ""){
+        window.alert("Please we'll love your input");
+        text.focus();
+        return false
+    }
+    return true
 }
